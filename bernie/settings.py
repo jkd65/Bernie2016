87074b4d6a2c9cde68d5797bd6492e1d52c098fa
Keevan Dance <Keevandance@nau.edu>
2015-07-20 12:52:36 -0700

    Revert "changed to mysql"
    This reverts commit f65161437805e4d3694e203dd538a5d6e332b4ea.

--- bernie/settings.py
+++ bernie/settings.py
@@ -78,10 +78,8 @@ WSGI_APPLICATION = 'bernie.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bernie2016',
-        'USER': 'bernie2016user',
-        'PASSWORD': 'johnda123',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
